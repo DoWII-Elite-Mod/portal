f3ab5f97e85f41a26bd9af8e12e499f31f5ad914
Grzegorz Sztuczyński <19194188+farce1@users.noreply.github.com>
2022-11-12 18:59:55 +0100

    feat: add boilerplate for portal frontend

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+  site_name: 'Elite mod portal',
+  title: 'Elite mod portal',
+  description: 'Elite mod',
+  author: 'Elite mod dev team',
+  locale_region: 'en-us',
+  locale: 'en'
+};
