d8d2085f1942bdb365f136f18beb114a56771d20
Grzegorz Sztuczyński <19194188+farce1@users.noreply.github.com>
2023-01-25 23:35:43 +0100

    chore: fix build and refactor astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import robotsTxt from 'astro-robots-txt';
-import { astroImageTools } from 'astro-imagetools';
 import image from '@astrojs/image';
 import vercel from '@astrojs/vercel/serverless';
 
@@ -32,9 +29,6 @@ export default defineConfig({
   integrations: [
     react(),
     tailwind({}),
-    sitemap(),
-    robotsTxt(),
-    astroImageTools,
     image({
       serviceEntryPoint: '@astrojs/image/sharp'
     })
--- src/shared/consts/dow_icons.ts
+++ src/shared/consts/dow_icons.ts
@@ -1,5 +1,5 @@
-import watchIcon from '../../../public/static/ran_improved_focus_large.png';
-import playIcon from '../../../public/static/sta_terminator_honors_large.png';
-import competeIcon from '../../../public/static/str_power_sword_aptit_large.png';
-
-export const dowIcons = [watchIcon, playIcon, competeIcon];
+export const dowIcons = [
+  '/images/ran_improved_focus_large.png',
+  '/images/sta_terminator_honors_large.png',
+  '/images/str_power_sword_aptit_large.png'
+];
